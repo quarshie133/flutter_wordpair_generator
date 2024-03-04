21fe6ec33d0a83e90dd664915cae5b8f6e0f0c63
quarshie133 <wrightsprince@gmail.com>
2024-03-04 14:09:24 +0000

    added appBar widget and body

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: ),
-      )
-    );
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: Colors.purple[900],
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('WordPair Generator'),
+            backgroundColor: Colors.purple[900],
+            foregroundColor: Colors.white,
+          ),
+          body: const Center(
+            child: Text('Hello World'),
+          ),
+        ));
   }
 }
