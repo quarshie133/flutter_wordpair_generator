dfee38a16bdea26b4bb5633bcbf93a029c23b795
quarshie133 <wrightsprince@gmail.com>
2024-03-04 23:53:38 +0000

    added statefulwidget for handling favorite icon and adding and removing data

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import './random_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,46 +12,5 @@ class MyApp extends StatelessWidget {
           primaryColor: Colors.purple[900],
         ),
         home: RandomWords());
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  State<RandomWords> createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  final _randomWordPairs = <WordPair>[];
-  Widget _buildList() {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, item) {
-        if (item.isOdd) return Divider();
-
-        final index = item ~/ 2;
-
-        if (index >= _randomWordPairs.length) {
-          _randomWordPairs.addAll(
-            generateWordPairs().take(10),
-          );
-        }
-        return _buildRow(_randomWordPairs[index]);
-      },
-    );
-  }
-
-  Widget _buildRow(WordPair pair) {
-    return ListTile(title: Text(pair.asPascalCase, style: TextStyle(fontSize: 18.0)));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('WordPair Generator'),
-          backgroundColor: Colors.purple[900],
-          foregroundColor: Colors.white,
-        ),
-        body: _buildList());
   }
 }
---
+++ lib/random_words.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+class RandomWords extends StatefulWidget {
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _randomWordPairs = <WordPair>[];
+  final _savedWordPairs = Set<WordPair>();
+
+  Widget _buildList() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, item) {
+        if (item.isOdd) return Divider();
+
+        final index = item ~/ 2;
+
+        if (index >= _randomWordPairs.length) {
+          _randomWordPairs.addAll(
+            generateWordPairs().take(10),
+          );
+        }
+        return _buildRow(_randomWordPairs[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _savedWordPairs.contains(pair);
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: TextStyle(fontSize: 18.0),
+      ),
+      trailing: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.red : null),
+      onTap: () {
+        setState(
+          () {
+            if (alreadySaved) {
+              _savedWordPairs.remove(pair);
+            } else {
+              _savedWordPairs.add(pair);
+            }
+          },
+        );
+      },
+    );
+  }
+
+  void _pushedSaved() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('WordPair Generator'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.list),
+              onPressed: _pushedSaved,
+              iconSize: 30,
+            )
+          ],
+          backgroundColor: Colors.purple[900],
+          foregroundColor: Colors.white,
+        ),
+        body: _buildList());
+  }
+}
